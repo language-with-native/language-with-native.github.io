67f1052552986311d667bdb4e857aeac70c99913
illia <i.v.golovach@hotmail.com>
2025-01-18 14:47:05 +0100

    Increase size

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,7 +1,7 @@
 export default function Subscribe() {
     return (
       <main>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfytz7gAwwyQ8TPDwUbiZ1K2m66N4UW-cycjGRPjuQc12nitg/viewform?embedded=true" width="100%" height="1000px">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfytz7gAwwyQ8TPDwUbiZ1K2m66N4UW-cycjGRPjuQc12nitg/viewform?embedded=true" width="100%" height="1200px">Loading…</iframe>
       </main>
     )
   }
