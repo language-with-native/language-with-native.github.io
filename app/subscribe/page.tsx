50d88356e3adff6c86c0b4540a3170ae97c7f846
illia <i.v.golovach@hotmail.com>
2025-01-18 13:40:30 +0100

    new form

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,9 +1,7 @@
 export default function Subscribe() {
     return (
       <main>
-        
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQrn58duC2t7Ln-mc01x5zLnfHILaxzwIPr-A2eL9qTLL6pw/viewform?embedded=true" width="100%" height="1000px">Loading…</iframe>
-        {/* <Companies /> */}
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfytz7gAwwyQ8TPDwUbiZ1K2m66N4UW-cycjGRPjuQc12nitg/viewform?embedded=true" width="100%" height="1000px">Loading…</iframe>
       </main>
     )
   }
